76c082b45ba58d2f5bee5e09974d9f15be1fafe6
kai-yamashita <kai-yama12345679@g.ecc.u-tokyo.ac.jp>
2022-12-08 19:41:47 +0900

    動的UIの導入

--- lib/main.dart
+++ lib/main.dart
@@ -22,43 +22,74 @@ class MyApp extends StatelessWidget {
 
 ///////////////////////////////
 // ③ アプリのメインページ（固定）
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  List<Widget> cards = [];
+  MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var cnt = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Menu Bar"),
-        ),
-        body: Center(
-          child: ListView(
-            children: [
-              TodoCardWidget(label: "option1"),
-              TodoCardWidget(label: "option2"),
-              TodoCardWidget(label: "option3"),
-              TodoCardWidget(label: "option4")
-            ],
-          ),
-        ));
+      appBar: AppBar(title: Text("Menu Bar")),
+      body: Center(
+          child: ListView.builder(
+        itemCount: widget.cards.length,
+        itemBuilder: (BuildContext context, int index) {
+          return widget.cards[index];
+        },
+      )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            widget.cards.add(TodoCardWidget(label: "${cnt}"));
+            if (cnt == 10) {
+              cnt = 0;
+              widget.cards = [];
+            }
+            cnt += 1;
+          });
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
 
-class TodoCardWidget extends StatelessWidget {
+class TodoCardWidget extends StatefulWidget {
   final String label;
+  var state = false;
 
-  TodoCardWidget({Key? key, required this.label});
+  TodoCardWidget({Key? key, required this.label}) : super(key: key);
+
+  @override
+  _TodoCardWidgetState createState() => _TodoCardWidgetState();
+}
+
+class _TodoCardWidgetState extends State<TodoCardWidget> {
+  void _changeState(value) {
+    setState(() {
+      widget.state = value ?? false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Card(
-        margin: EdgeInsets.all(10),
-        child: Container(
-          padding: EdgeInsets.all(10),
-          child: Row(
-            children: [
-              Checkbox(value: false, onChanged: null),
-              Text(this.label),
-            ],
-          ),
-        ));
+      margin: EdgeInsets.all(10),
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          children: [
+            Checkbox(onChanged: _changeState, value: widget.state),
+            Text(widget.label),
+          ],
+        ),
+      ),
+    );
   }
 }
