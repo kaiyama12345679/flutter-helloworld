c86f59381d256dde5b1232098c50a486d898d42d
kai-yamashita <kai-yama12345679@g.ecc.u-tokyo.ac.jp>
2022-12-09 17:12:09 +0900

    jsonを用いた状態保存

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -20,7 +22,6 @@ class MyApp extends StatelessWidget {
 ///////////////////////////////
 // ③ アプリのメインページ（固定）
 class MyHomePage extends StatefulWidget {
-  List<Widget> cards = [];
   MyHomePage({Key? key}) : super(key: key);
 
   @override
@@ -30,16 +31,20 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
-    //Widgetを初期化した時の処理をoverride
     super.initState();
-    SharedPreferences.getInstance().then((pref) {
-      var todo = pref.getStringList("todo") ?? [];
-      for (var v in todo) {
-        setState(() {
-          widget.cards.add(TodoCardWidget(label: v));
-        });
-      }
-    });
+  }
+
+  Future<List<dynamic>> getCards() async {
+    var prefs = await SharedPreferences.getInstance();
+    List<Widget> cards = [];
+    var todo = prefs.getStringList("todo") ?? [];
+    for (var jsonStr in todo) {
+      var mapObj = jsonDecode(jsonStr);
+      var title = mapObj["title"];
+      var state = mapObj["state"];
+      cards.add(TodoCardWidget(label: title, state: state));
+    }
+    return cards;
   }
 
   @override
@@ -52,35 +57,49 @@ class _MyHomePageState extends State<MyHomePage> {
               onPressed: () {
                 SharedPreferences.getInstance().then((pref) async {
                   await pref.setStringList("todo", []);
-                  setState(() {
-                    widget.cards = [];
-                  });
+                  setState(() {});
                 });
               },
               icon: const Icon(Icons.delete))
         ],
       ),
       body: Center(
-        child: ListView.builder(
-          itemCount: widget.cards.length,
-          itemBuilder: (BuildContext context, int index) {
-            return widget.cards[index];
-          },
-        ),
-      ),
+          child: FutureBuilder<List>(
+        future: getCards(),
+        builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              return const Text("Waiting to start");
+            case ConnectionState.waiting:
+              return const Text("Loading...");
+            default:
+              if (snapshot.hasError) {
+                return Text("Error: ${snapshot.error}");
+              } else {
+                return ListView.builder(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return snapshot.data![index];
+                  },
+                );
+              }
+          }
+        },
+      )),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           var label = await _showTextInputDialog(context);
 
           if (label != null) {
-            setState(() {
-              widget.cards.add(TodoCardWidget(label: label));
-            });
-
             SharedPreferences pref = await SharedPreferences.getInstance();
             var todo = pref.getStringList("todo") ?? [];
-            todo.add(label);
+
+            var mapObj = {"title": label, "state": false};
+            var jsonStr = jsonEncode(mapObj);
+            todo.add(jsonStr);
             await pref.setStringList("todo", todo);
+
+            setState(() {});
             _textFieldController.text = "";
           }
         },
@@ -121,17 +140,31 @@ class TodoCardWidget extends StatefulWidget {
   final String label;
   var state = false;
 
-  TodoCardWidget({Key? key, required this.label}) : super(key: key);
+  TodoCardWidget({Key? key, required this.label, required this.state})
+      : super(key: key);
 
   @override
   _TodoCardWidgetState createState() => _TodoCardWidgetState();
 }
 
 class _TodoCardWidgetState extends State<TodoCardWidget> {
-  void _changeState(value) {
+  void _changeState(value) async {
     setState(() {
       widget.state = value ?? false;
     });
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    var todo = prefs.getStringList("todo") ?? [];
+
+    for (int i = 0; i < todo.length; i++) {
+      var mapObj = jsonDecode(todo[i]);
+      if (mapObj["title"] == widget.label) {
+        mapObj["state"] = widget.state;
+        todo[i] = jsonEncode(mapObj);
+      }
+    }
+
+    prefs.setStringList("todo", todo);
   }
 
   @override
